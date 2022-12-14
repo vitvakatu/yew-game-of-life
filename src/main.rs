5a9ed207af9421d561ee5343e5ae7cc6ea687ee6
Ilya Bogdanov <fumlead@gmail.com>
2022-12-14 23:01:47 +0400

    Add Click and StartStop

--- src/main.rs
+++ src/main.rs
@@ -29,9 +29,20 @@ impl App {
     }
 
     fn live_neighbor_count(&self, row: u32, column: u32) -> u8 {
-        // Реализуйте эту функцию.
-        // Возвращает количество Alive клеток вокруг клетки (`row`, `column`).
-        todo!()
+        let mut count = 0;
+        for delta_row in [self.height - 1, 0, 1].iter().cloned() {
+            for delta_col in [self.width - 1, 0, 1].iter().cloned() {
+                if delta_row == 0 && delta_col == 0 {
+                    continue;
+                }
+
+                let neighbor_row = (row + delta_row) % self.height;
+                let neighbor_col = (column + delta_col) % self.width;
+                let idx = self.get_index(neighbor_row, neighbor_col);
+                count += self.cells[idx] as u8;
+            }
+        }
+        count
     }
 
     pub fn new(interval: Option<Interval>) -> App {
@@ -59,31 +70,61 @@ impl App {
     }
 
     pub fn tick(&mut self) {
-        // Реализуйте эту функцию.
+        let mut next = self.cells.clone();
 
-        // Rule 1: Любая живая клетка с менее чем двумя соседями умирает
-        // Rule 2: Любая живая клетка с двумя или тремя соседями живет дальше
-        // Rule 3: Любая живая клетка с более чем тремя соседями умирает
-        // Rule 4: Любая мертвая клетка с тремя соседями становится живой
-        // Все остальные клетки остаются в том же состоянии.
+        for row in 0..self.height {
+            for col in 0..self.width {
+                let idx = self.get_index(row, col);
+                let cell = self.cells[idx];
+                let live_neighbors = self.live_neighbor_count(row, col);
 
-        todo!();
+                let next_cell = match (cell, live_neighbors) {
+                    // Rule 1: Any live cell with fewer than two live neighbours
+                    // dies, as if caused by underpopulation.
+                    (Cell::Alive, x) if x < 2 => Cell::Dead,
+                    // Rule 2: Any live cell with two or three live neighbours
+                    // lives on to the next generation.
+                    (Cell::Alive, 2) | (Cell::Alive, 3) => Cell::Alive,
+                    // Rule 3: Any live cell with more than three live
+                    // neighbours dies, as if by overpopulation.
+                    (Cell::Alive, x) if x > 3 => Cell::Dead,
+                    // Rule 4: Any dead cell with exactly three live neighbours
+                    // becomes a live cell, as if by reproduction.
+                    (Cell::Dead, 3) => Cell::Alive,
+                    // All other cells remain in the same state.
+                    (otherwise, _) => otherwise,
+                };
+
+                next[idx] = next_cell;
+            }
+        }
+
+        self.cells = next;
+    }
+
+    fn create_interval(ctx: &Context<App>) -> Interval {
+        let callback = ctx.link().callback(|_| Msg::Tick);
+        let interval = Interval::new(200, move || callback.emit(()));
+        interval
     }
 }
 
-fn view_cell(idx: usize, cell: &Cell, _link: &Scope<App>) -> Html {
+fn view_cell(idx: usize, cell: &Cell, link: &Scope<App>) -> Html {
     let status = match cell {
         Cell::Alive => "cellule-live",
         Cell::Dead => "cellule-dead",
     };
+    let onclick = link.callback(move |_| Msg::Click(idx));
 
     html! {
-        <div key={idx} class={classes!("game-cellule", status)}></div>
+        <div key={idx} class={classes!("game-cellule", status)} onclick={onclick}></div>
     }
 }
 
 pub enum Msg {
     Tick,
+    StartStop,
+    Click(usize),
 }
 
 impl Component for App {
@@ -91,15 +132,26 @@ impl Component for App {
     type Properties = ();
 
     fn create(ctx: &Context<Self>) -> Self {
-        let callback = ctx.link().callback(|_| Msg::Tick);
-        let interval = Interval::new(200, move || callback.emit(()));
+        let interval = Self::create_interval(ctx);
 
         Self::new(Some(interval))
     }
 
-    fn update(&mut self, _ctx: &Context<Self>, msg: Self::Message) -> bool {
+    fn update(&mut self, ctx: &Context<Self>, msg: Self::Message) -> bool {
         match msg {
             Msg::Tick => self.tick(),
+            Msg::StartStop => {
+                if let Some(_) = self._interval.take() {
+                } else {
+                    self._interval = Some(Self::create_interval(ctx));
+                }
+            }
+            Msg::Click(idx) => {
+                self.cells[idx] = match self.cells[idx] {
+                    Cell::Alive => Cell::Dead,
+                    Cell::Dead => Cell::Alive,
+                };
+            }
         }
         true
     }
@@ -118,6 +170,7 @@ impl Component for App {
                     </div>
                 }
             });
+        let onclick = ctx.link().callback(|_| Msg::StartStop);
         html! {
             <div>
                 <section class="game-container">
@@ -129,6 +182,7 @@ impl Component for App {
                             { for cell_rows }
                         </div>
                     </section>
+                    <button onclick={onclick}>{ "Start/Stop" }</button>
                 </section>
             </div>
         }
