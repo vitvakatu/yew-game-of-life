fe90550ad7dbe67712d5c80e439872664bb107c4
Ilya Bogdanov <fumlead@gmail.com>
2022-12-14 19:12:38 +0400

    Prepare for lesson

--- src/main.rs
+++ src/main.rs
@@ -19,7 +19,7 @@ pub enum Cell {
 pub struct App {
     width: u32,
     height: u32,
-    _interval: Interval,
+    _interval: Option<Interval>,
     cells: Vec<Cell>,
 }
 
@@ -29,23 +29,12 @@ impl App {
     }
 
     fn live_neighbor_count(&self, row: u32, column: u32) -> u8 {
-        let mut count = 0;
-        for delta_row in [self.height - 1, 0, 1].iter().cloned() {
-            for delta_col in [self.width - 1, 0, 1].iter().cloned() {
-                if delta_row == 0 && delta_col == 0 {
-                    continue;
-                }
-
-                let neighbor_row = (row + delta_row) % self.height;
-                let neighbor_col = (column + delta_col) % self.width;
-                let idx = self.get_index(neighbor_row, neighbor_col);
-                count += self.cells[idx] as u8;
-            }
-        }
-        count
+        // Реализуйте эту функцию.
+        // Возвращает количество Alive клеток вокруг клетки (`row`, `column`).
+        todo!()
     }
 
-    pub fn new(interval: Interval) -> App {
+    pub fn new(interval: Option<Interval>) -> App {
         let width = 64;
         let height = 64;
 
@@ -70,45 +59,19 @@ impl App {
     }
 
     pub fn tick(&mut self) {
-        let mut next = self.cells.clone();
+        // Реализуйте эту функцию.
 
-        for row in 0..self.height {
-            for col in 0..self.width {
-                let idx = self.get_index(row, col);
-                let cell = self.cells[idx];
-                let live_neighbors = self.live_neighbor_count(row, col);
+        // Rule 1: Любая живая клетка с менее чем двумя соседями умирает
+        // Rule 2: Любая живая клетка с двумя или тремя соседями живет дальше
+        // Rule 3: Любая живая клетка с более чем тремя соседями умирает
+        // Rule 4: Любая мертвая клетка с тремя соседями становится живой
+        // Все остальные клетки остаются в том же состоянии.
 
-                let next_cell = match (cell, live_neighbors) {
-                    // Rule 1: Any live cell with fewer than two live neighbours
-                    // dies, as if caused by underpopulation.
-                    (Cell::Alive, x) if x < 2 => Cell::Dead,
-                    // Rule 2: Any live cell with two or three live neighbours
-                    // lives on to the next generation.
-                    (Cell::Alive, 2) | (Cell::Alive, 3) => Cell::Alive,
-                    // Rule 3: Any live cell with more than three live
-                    // neighbours dies, as if by overpopulation.
-                    (Cell::Alive, x) if x > 3 => Cell::Dead,
-                    // Rule 4: Any dead cell with exactly three live neighbours
-                    // becomes a live cell, as if by reproduction.
-                    (Cell::Dead, 3) => Cell::Alive,
-                    // All other cells remain in the same state.
-                    (otherwise, _) => otherwise,
-                };
-
-                next[idx] = next_cell;
-            }
-        }
-
-        self.cells = next;
+        todo!();
     }
 }
 
-
-
-
-/// Prepared code
-
-fn view_cell(idx: usize, cell: &Cell, link: &Scope<App>) -> Html {
+fn view_cell(idx: usize, cell: &Cell, _link: &Scope<App>) -> Html {
     let status = match cell {
         Cell::Alive => "cellule-live",
         Cell::Dead => "cellule-dead",
@@ -131,7 +94,7 @@ impl Component for App {
         let callback = ctx.link().callback(|_| Msg::Tick);
         let interval = Interval::new(200, move || callback.emit(()));
 
-        Self::new(interval)
+        Self::new(Some(interval))
     }
 
     fn update(&mut self, _ctx: &Context<Self>, msg: Self::Message) -> bool {
@@ -169,5 +132,33 @@ impl Component for App {
                 </section>
             </div>
         }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_live_neighbor_count() {
+        let mut app = App::new(None);
+        app.cells = vec![Cell::Dead; 64 * 64];
+        for i in 0..64 {
+            for j in 0..64 {
+                assert_eq!(app.live_neighbor_count(i, j), 0);
+            }
+        }
+        // Верхний левый угол
+        app.cells[0] = Cell::Alive;
+        assert_eq!(app.live_neighbor_count(0, 0), 0);
+        app.cells[1] = Cell::Alive;
+        assert_eq!(app.live_neighbor_count(0, 0), 1);
+        // Поле замкнуто со всех сторон (представляет собой поверхность тора)
+        assert_eq!(app.live_neighbor_count(63, 0), 2);
+        assert_eq!(app.live_neighbor_count(0, 63), 1);
+        assert_eq!(app.live_neighbor_count(63, 63), 1);
+        // Правый верхний угол
+        app.cells[63] = Cell::Alive;
+        assert_eq!(app.live_neighbor_count(1, 63), 2);
     }
 }
